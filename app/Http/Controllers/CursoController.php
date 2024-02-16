b049e3e7bc9dfb68658b5321acf442c4d32235e3
cristianyork20 <cristiansarria658@gmail.com>
2024-02-16 17:11:36 -0500

    primer subida

---
+++ app/Http/Controllers/CursoController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CursoController extends Controllers
+{
+    public function hola(){
+
+        return 'hola hijitos';
+
+    }
+
+}
